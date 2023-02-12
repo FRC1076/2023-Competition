83f2e9caf703071adce18522f6259c39e55c88ff
Cryplo <dylanliaa@gmail.com>
2023-02-12 14:17:40 -0500

    Switch kOff to kReverse

--- grabber.py
+++ grabber.py
@@ -9,12 +9,13 @@ class Grabber:
         self.left_motor = rev.CANSparkMax(left_id, motor_type)
         self.solenoid = wpilib.DoubleSolenoid(0, solenoid_forward_id, solenoid_reverse_id)
 
+    #1.00917431193 inches per rotation
     def extend(self, value):
         self.right_motor.set(value)
         self.left_motor.set(-value)
 
     def toggle(self):
         if self.solenoid.get() == DoubleSolenoid.Value.kForward:
-            self.solenoid.set(DoubleSolenoid.Value.kOff)
-        if self.solenoid.get() == DoubleSolenoid.Value.kOff:
+            self.solenoid.set(DoubleSolenoid.Value.kReverse)
+        if self.solenoid.get() == DoubleSolenoid.Value.kReverse:
             self.solenoid.set(DoubleSolenoid.Value.kForward)
