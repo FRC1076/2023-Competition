27b6b3e0d5ee519ded05f0323f338526e2de179c
Cryplo <dylanliaa@gmail.com>
2023-03-03 10:32:32 -0500

    Allow control arm to move to preset destination using button presses, and make moveToPos more accurate

--- grabber.py
+++ grabber.py
@@ -12,42 +12,37 @@ class Grabber:
         self.left_motor = rev.CANSparkMax(left_id, motor_type)
         self.right_encoder = self.right_motor.getEncoder()
         self.left_encoder = self.left_motor.getEncoder()
-        #self.right_encoder.setPosition(0)
-        #self.left_encoder.setPosition(0)
         self.solenoid = wpilib.DoubleSolenoid(1, wpilib.PneumaticsModuleType.REVPH, solenoid_forward_id, solenoid_reverse_id)
         self.pid_controller = PIDController(0.5, 0.00001, 0.00001) #change later
-        #self.pid_controller.setTolerance(0.005)
-        #assume retracted
-        #self.right_motor.getEncoder().setZeroOffset(self.right_motor.getEncoder().getPosition())
-        #self.left_motor.getEncoder().setZeroOffset(self.left_motor.getEncoder().getPosition())
+        self.pid_controller.setTolerance(0.00005)
 
     #1.00917431193 inches per rotation
     def extend(self, value):
-        print("Value:", value)
+        print(self.getEncoderPosition())
         if value > 1:
             value = 1
         if value < -1:
             value = -1
         #make sure arm doesn't go past limit
-        if self.right_encoder.getPosition() > 36 and value < 0:
+        if self.getEncoderPosition() > 36 and value < 0:
             self.right_motor.set(0)
             self.left_motor.set(0)
             return
-        if self.right_encoder.getPosition() < 1 and value > 0:
+        if self.getEncoderPosition() < 1 and value > 0:
             self.right_motor.set(0)
             self.left_motor.set(0)
             return
+        #set to a tenth of the power
         self.right_motor.set(-value * 0.1)
         self.left_motor.set(-value * 0.1)
 
+    #automatically move to a position using a pid controller
     def moveToPos(self, value):
-        extend_value = self.pid_controller.calculate(self.right_encoder.getPosition(), value)
-        if(self.pid_controller.atSetpoint()):
-            print("Reached")
+        extend_value = self.pid_controller.calculate(self.getEncoderPosition(), value)
+        if(abs(extend_value * 0.3) < 0.01):
             self.extend(0)
             return True
         else:
-            print("Moving")
             self.extend(-extend_value * 3)
             return False
 
