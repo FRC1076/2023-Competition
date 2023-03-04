3ec73bd8e6cee59ed842ab81ac6af083d24bf22d
Mark Newman <mwnewman@umich.edu>
2023-03-04 12:43:33 -0500

    made grabber stay grabby

--- grabber.py
+++ grabber.py
@@ -6,12 +6,22 @@ class Grabber:
     def __init__(self, motor_id):
         motor_type = rev.CANSparkMaxLowLevel.MotorType.kBrushless
         self.motor = rev.CANSparkMax(motor_id, motor_type) # elevator up-down
+        self.isEngaged = False
 
-    def grab(self):
-        self.motor.set(1)
-        print('Grabber is grabbing!')
+    def engage(self):
+        self.isEngaged = True
+        return True # task is complete
 
     def release(self):
-        self.motor.set(0)
-        print('Grabber is releasing!')
+        self.isEngaged = False
+        return True # task is complete
+    
+    def toggle(self):
+        self.isEngaged = not self.isEngaged
+        return True # probably not needed?
 
+    def execute(self):
+        if self.isEngaged:
+            self.motor.set(1)
+        else:
+            self.motor.set(0)
